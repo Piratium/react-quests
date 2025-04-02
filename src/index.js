0699c17bfdbba421feb06106c2d560cd53d57c79
Paul <piratium3@gmail.com>
2025-04-02 19:58:25 +0300

    Рендер элементов циклом

--- src/index.js
+++ src/index.js
@@ -4,16 +4,24 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const value = "Общий план моего роста"
-const items = ["Потрогать реакт, понять как его использовать и зачем", "Отдать всё время на бэк, в частности .NET;", "Попасть в ряды бэкендеров."]
+const value = "Общий план моего роста";
+const items = ["Потрогать реакт, понять как его использовать и зачем", "Отдать всё время на бэк, в частности .NET;", "Попасть в ряды бэкендеров."];
+const now = new Date();
+
+function getItems(array) {
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    array[i] = <li>{array[i]}</li>;
+  }
+  return array;
+}
 
 root.render(
   <div>
     <h1>{value}</h1>
     <ul>
-      <li>{items[0]}</li>
-      <li>{items[1]}</li>
-      <li>{items[2]}</li>
+        {getItems(items)}
     </ul>
+    <p>&#169; Авторские права мои {now.getFullYear()}</p>
   </div>
 );
