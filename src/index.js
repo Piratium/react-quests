568464819cc36cc9118eaacaa54ddd5fcfacf7ea
Paul <piratium3@gmail.com>
2025-04-02 20:02:03 +0300

    Рендер массива через map()

--- src/index.js
+++ src/index.js
@@ -8,19 +8,11 @@ const value = "Общий план моего роста";
 const items = ["Потрогать реакт, понять как его использовать и зачем", "Отдать всё время на бэк, в частности .NET;", "Попасть в ряды бэкендеров."];
 const now = new Date();
 
-function getItems(array) {
-  for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    array[i] = <li>{array[i]}</li>;
-  }
-  return array;
-}
-
 root.render(
   <div>
     <h1>{value}</h1>
     <ul>
-        {getItems(items)}
+        {items.map(item => <li>{item}</li>)}
     </ul>
     <p>&#169; Авторские права мои {now.getFullYear()}</p>
   </div>
