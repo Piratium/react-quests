887991af9c850b7319c5e9d12fac94953d192176
Paul <piratium3@gmail.com>
2025-04-02 19:25:42 +0300

    my plan

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <h1>Hello, World!</h1>
+  <div>
+    <h1>Общий план моего роста</h1>
+    <ul>
+      <li>Потрогать реакт, понять как его использовать и зачем;</li>
+      <li>Отдать всё время на бэк, в частности .NET;</li>
+      <li>Попасть в ряды бэкендеров.</li>
+    </ul>
+  </div>
 );
