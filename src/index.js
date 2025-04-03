a31d4535426a3f2323c249cbae581ff00cc44d4c
Paul <piratium3@gmail.com>
2025-04-03 12:28:42 +0300

    react + bootstrap

--- src/index.js
+++ src/index.js
@@ -4,16 +4,26 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const value = "Общий план моего роста";
-// const items = ["Потрогать реакт, понять как его использовать и зачем", "Отдать всё время на бэк, в частности .NET;", "Попасть в ряды бэкендеров."];
+const value = "Карточки";
 const now = new Date();
 
 root.render(
-  <div>
-    <h1>{value}</h1>
-    <ul>
-        {Array.from(["Потрогать реакт, понять как его использовать и зачем", "Отдать всё время на бэк, в частности .NET;", "Попасть в ряды бэкендеров."], item => <li>{item}</li>)}
-    </ul>
-    <p>&#169; Авторские права мои {now.getFullYear()}</p>
+  <div className='container col-10'>
+    <h1 className='text-center mb-5'>{value}</h1>
+    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+    {Array.from({length: 5}, (_,index) => 
+      <div key={index} className="col">
+        <div className="card">
+          <img src={`https://cataas.com/cat?width=500&height=${300+index}`} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>)}
+    </div>
+    <p className='text-center mt-5'>&#169; Авторские права мои {now.getFullYear()}</p>
+    
   </div>
 );
